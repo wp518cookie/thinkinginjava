b11f2c0d8255ad5e80b3f4aeaa8b2c939e2f8222
robin <403704156@qq.com>
2016-12-01 18:02:26 +0800

    2016/12/1

---
+++ src/chapter17_Container/SlowMap.java
@@ -0,0 +1,49 @@
+package chapter17_Container;
+
+import net.mindview.util.Countries;
+
+import java.util.*;
+
+/**
+ * Created by ping.wu on 2016/12/1.
+ */
+public class SlowMap<K, V> extends AbstractMap<K, V> {
+    private List<K> keys = new ArrayList<>();
+    private List<V> values = new ArrayList<>();
+
+    public V put(K key, V value) {
+        V oldValue = get(key);
+        if (!keys.contains(key)) {
+            keys.add(key);
+            values.add(value);
+        } else {
+            values.set(keys.indexOf(key), value);
+        }
+        return oldValue;
+    }
+
+    public V get(Object key) {
+        if (!keys.contains(key)) {
+            return null;
+        }
+        return values.get(keys.indexOf(key));
+    }
+
+    public Set<Map.Entry<K, V>> entrySet() {
+        Set<Map.Entry<K, V>> set = new HashSet<>();
+        Iterator<K> ki = keys.iterator();
+        Iterator<V> vi = values.iterator();
+        while (ki.hasNext()) {
+            set.add(new MapEntry<K, V>(ki.next(), vi.next()));
+        }
+        return set;
+    }
+
+    public static void main(String[] args) {
+        SlowMap<String, String> m = new SlowMap<String, String>();
+        m.putAll(Countries.capitals(15));
+        System.out.println(m);
+        System.out.println(m.get("BULGARIA"));
+        System.out.println(m.entrySet());
+    }
+}
